e147d7f5fb5b3c7df601a926cbb18dd4aa0e5161
Joe Sarif <joesarif@gmail.com>
2023-03-08 20:39:37 +0000

    add new projects

---
+++ Magic-8-Ball/magic-8-ball.js
@@ -0,0 +1,69 @@
+// variable for creating a username
+let userName = 'Joe';
+
+// Checks for a username
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello');
+
+// variable for entering a question
+const userQuestion = "Will I get lot's of Xmas presents this year?";
+
+//logs the question
+console.log(`${userName} has asked - ${userQuestion}`);
+
+//creates a random number between 0-7
+let randomNumber = Math.floor(Math.random() *8);
+
+let eightBall = '';
+
+// Asigns random number to a string
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+
+/* Using else if
+
+if (randomNumber === 0) {
+  eightBall = 'It is certain';
+} else if (randomNumber === 1) {
+  eightBall = 'It is decidedly so';
+} else if (randomNumber === 2) {
+  eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3) {
+  eightBall = 'Cannot predict now';
+} else if (randomNumber === 4) {
+  eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+  eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+  eightBall = 'Outlook not so good';
+} else if (randomNumber === 7) {
+  eightBall = 'Signs point to yes';
+} 
+*/
+
+// prints eightball answer to console
+console.log(eightBall)
